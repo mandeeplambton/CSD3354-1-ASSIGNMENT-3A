3f4788533a40f9ceb419053e09a055b2a2ae31ea
visualstudio <visualstudio@230-PC45>
2019-03-14 16:00:47 -0400

    COMMIT2

---
+++ BankTests/BankAccountTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccount;
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        public BankAccountTests(string v, double beginningBalance)
+        {
+        }
+
+        public double DebitBalance { get; private set; }
+
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance(double debitAmount)
+        {
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccountTests account = new BankAccountTests("Mr. Bryan Walton", beginningBalance);
+            //Act
+            account.Debit_WithValidAmount_UpdatesBalance(debitAmount);
+
+            //Assert
+            double actual = account.DebitBalance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+    }
+}
---
+++ BankTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f8b6b226-5ac9-4cc8-b030-14110ee8b2e0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
